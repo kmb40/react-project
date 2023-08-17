df9ae0dca13f906b2e855d0e34e85c6f2647b281
Kyle M. Brown <mayvik@gmail.com>
2023-08-17 06:26:34 -0400

    Added list

--- index.js
+++ index.js
@@ -1,1 +1,3 @@
-ReactDOM.render(<p>Hi, my name is KB!</p>, document.getElementById("root"))
+
+
+ReactDOM.render(<p>Hi, my name is KB!<ol><li>List 1</li></ol><ul><li>list 2</li></ul></p>, document.getElementById("root"))

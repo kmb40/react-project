bd49129a0f158f596a8da2e230f276d59f16f40d
Kyle M. Brown <mayvik@gmail.com>
2023-08-29 11:31:19 -0400

    Added node and npm to local env

---
+++ src/header.js
@@ -0,0 +1,22 @@
+import React from "react"
+
+export default function Header(){
+    return (
+        <header>
+             <nav className="nav">
+                <img className="nav-logo" src="react-logo.png" />
+                  <ul className="nav-items">
+                    <li>
+                        <a href="#">Pricing</a>
+                    </li>
+                    <li>
+                        <a href="#">About</a>
+                    </li>
+                    <li>
+                        <a href="#">Contact</a>
+                    </li>
+                  </ul>
+             </nav>
+        </header>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/*
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -15,3 +16,47 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+*/
+
+import React from "react"
+import ReactDOM from "react-dom"
+import Header from "./header"
+
+function PageBody() {
+    return(
+        <div>
+            <h1>Reasons that I am learning REACTjs</h1>
+                <ol>
+                    <li>
+                        <a href="#">I love to learn.</a>
+                    </li>
+                    <li>
+                        <a href="#">To keep programming skills from going into cold storage.</a>
+                    </li>
+                    <li>
+                        <a href="#">To build on existing programming skills.</a>
+                    </li>
+                </ol>
+        </div>
+    )
+}
+
+
+function Footer(){
+    return(
+        <footer><small>© 2023 Brown development. All rights reserved.</small></footer>
+    )
+}
+
+function WhyIlearnPage(){
+    return (
+        <div>
+            <Header/>
+            <PageBody/>
+            <Footer />
+        </div>
+        )       
+    }
+    // Written on a single line
+
+ReactDOM.render(<WhyIlearnPage />, document.getElementById("root"))

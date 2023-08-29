741a77eef408a9a170731d0966bf224b6ce64f75
Kyle M. Brown <mayvik@gmail.com>
2023-08-29 12:35:41 -0400

    Removed duplicates after local merge

--- src/footer.js
+++ src/footer.js
@@ -2,6 +2,6 @@ import React from "react"
 
 export default function Footer(){
     return(
-        <footer><small>© 2023 Brown development. All rights reserved.</small></footer>
+        <footer><small>© 2023 Brown development. All rights reserved bitch.</small></footer>
     )
 }
